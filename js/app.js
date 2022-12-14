90843ee5bd979f14f5ac20d295fff843dd09927b
ErickFRG-01 <115059395+ErickFRG-01@users.noreply.github.com>
2022-12-13 23:47:38 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,19 +6,3 @@ if ("serviceWorker" in navigator) {
         .catch(err => console.log("service worker not registered", err))
     })
   }
-  const showCoffees = () => {
-  let output = ""
-  coffees.forEach(
-    ({ name, image }) =>
-      (output += `
-              <div class="card">
-                <img class="card--avatar" src=${image} />
-                <h1 class="card--title">${Dino proyect}</h1>
-                <a class="card--link" href="#">Taste</a>
-              </div>
-              `)
-  )
-  container.innerHTML = output
-}
-
-document.addEventListener("DOMContentLoaded", showCoffees)
